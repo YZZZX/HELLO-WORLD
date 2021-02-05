e0f32eba48e4992fd39942d83101594f67509920
liuchangwei <liuchangwei@cestc.cn>
2021-02-05 15:40:37 +0800

    yzx

--- src/com/lcw/HelloWorld.java
+++ src/com/lcw/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
     public static void main(String[] args) {
 
         System.out.printf("我是lcw创建的 ");
-        System.out.printf("master自己变更");
+        System.out.printf("yzx");
     }
 
 }
