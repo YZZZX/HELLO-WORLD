73b02f95dfc8df43b95c00a81e4a36bf012fda52
liuchangwei <liuchangwei@cestc.cn>
2021-02-05 15:20:03 +0800

    master自己变更

--- src/com/lcw/HelloWorld.java
+++ src/com/lcw/HelloWorld.java
@@ -2,6 +2,9 @@ package com.lcw;
 
 public class HelloWorld {
     public static void main(String[] args) {
+
         System.out.printf("我是lcw创建的 ");
+        System.out.printf("master自己变更");
     }
+
 }
